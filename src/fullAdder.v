0b67c9280cebb4b7dfa5571181a301b389e854cb
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2025-12-12 14:49:21 +0200

    init: initialzing project files

---
+++ src/fullAdder.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ps
+module full_adder(
+  input a,
+  input b,
+  input c_in,
+  output sum,
+  output c_out
+);
+  assign sum = a ^ b ^ c_in;
+  assign c_out = (a & b) | (a & c_in) | (c_in & b);
+endmodule

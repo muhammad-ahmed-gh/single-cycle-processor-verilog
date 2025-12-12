d9a8199e18659af71b22f9efe2ae5109a58b99b6
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2025-12-12 20:01:21 +0200

    feat: design ALU module

--- testBench.v
+++ testBench.v
@@ -1,19 +1,7 @@
 module test_bench;
-  reg a;
-  reg b;
-  reg c_in;
-  wire sum;
-  wire c_out;
-
-  full_adder fa (a, b, c_in, sum, c_out);
-
   initial begin
-    a = 1'b0;
-    b = 1'b0;
-    c_in = 1'b0;
+    
+    $display("Hello, world!");
 
-    $monitor("a: %d, b: %d, c in: %d, sum: %d, c out: %d",
-    a, b, c_in,
-    sum, c_out);
   end
 endmodule

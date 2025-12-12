5fa2703927f9ed326d45fdb82d7ab6dcbef4bf5e
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2025-12-12 20:04:52 +0200

    hotfix: fix syntax errors

--- src/ALU.v
+++ src/ALU.v
@@ -1,13 +1,13 @@
 `timescale 1ns/1ps
 module ALU(
-  input [63: 0] a, b
+  input [63: 0] a, b,
   input [3: 0] ALUControl,
   output reg [63: 0] result,
   output reg carryOut, zero
 );
 
   always @(*) begin
-    carryOut = 0
+    carryOut = 0;
 
     case (ALUControl)
       4'b0000: result = a & b;

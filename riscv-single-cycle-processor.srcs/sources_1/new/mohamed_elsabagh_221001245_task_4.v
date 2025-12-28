e7600351b2b311d686bc79790874b9dc3a92041e
elsabaghmedo04-cmyk <elsabagh.medo04@gmail.com>
2025-12-28 14:56:33 +0200

    Update mohamed_elsabagh_221001245_task_4.v

--- riscv-single-cycle-processor.srcs/sources_1/new/mohamed_elsabagh_221001245_task_4.v
+++ riscv-single-cycle-processor.srcs/sources_1/new/mohamed_elsabagh_221001245_task_4.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
-// Program Counter, Branch Logic & PC Update
-// You will design all components related to instruction sequencing:
-// • PC register (updates every cycle)
-// • PC + 4 adder
-// • Branch target calculation
-// • Branch decision logic
-// • MUX for selecting the next PC
-// • Handling branching correctly
 
-// Your responsibilities:
-// • Implement PC logic in Verilog
-// • Verify correct branching behavior in a testbench
-
-module pc(
-
+module pc (
+    input  wire        clk,
+    input  wire        reset,
+    input  wire        PCWrite,
+    input  wire [31:0] PC_in,
+    output reg  [31:0] PC_out
 );
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        PC_out <= 32'd0;
+    else if (PCWrite)
+        PC_out <= PC_in;
+end
+
 endmodule

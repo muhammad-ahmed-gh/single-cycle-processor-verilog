6cc7592c2700ec4e571983456b96f747142a970d
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2025-12-28 16:32:21 +0200

    adjust program counter width

--- riscv-single-cycle-processor.srcs/sources_1/new/mohamed_elsabagh_221001245_task_4.v
+++ riscv-single-cycle-processor.srcs/sources_1/new/mohamed_elsabagh_221001245_task_4.v
@@ -4,15 +4,15 @@ module PC (
     input  wire        clk,
     input  wire        reset,
     input  wire        PCWrite,
-    input  wire [31:0] PC_in,
-    output reg  [31:0] PC_out
+    input  wire [63:0] PC_in,
+    output reg  [63:0] PC_out
 );
 
-always @(posedge clk or posedge reset) begin
-    if (reset)
-        PC_out <= 32'd0;
-    else if (PCWrite)
-        PC_out <= PC_in;
-end
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            PC_out <= 64'd0;
+        else if (PCWrite)
+            PC_out <= PC_in;
+    end
 
 endmodule
